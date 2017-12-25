b3a8e8103a5957b48a544e3a52812c6d1ef06284
Vitaliy <vkhotey@ya.ru>
2017-12-25 20:20:03 +0200

    Update webpack.config.js

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -2,19 +2,22 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
+console.log(process.env.NODE_PATH);
 module.exports = {
-  entry: './src/index.js',
+  entry: [
+    'babel-polyfill',
+    path.resolve(__dirname, '../src/index.js'),
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, '../dist'),
     publicPath: '/',
   },
-  devServer: {
-    historyApiFallback: true,
-  },
   devtool: 'inline-source-map',
   plugins: [
-    new CleanWebpackPlugin(['../dist']),
+    new CleanWebpackPlugin([ path.resolve(__dirname, '../dist') ], {
+      root: process.env.NODE_PATH,
+    }),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '../bin/index.html'),
       inject: false,
@@ -23,12 +26,8 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /.css$/,
+        test: /\.css$/,
         use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /.js$/,
-        use: 'babel-loader',
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif|woff|woff2|eot|ttf|otf)$/,
@@ -37,9 +36,16 @@ module.exports = {
           {
             loader: 'url-loader',
             options: { limit: 40000 },
-          },
-          'image-webpack-loader',
-        ],
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: 'image-webpack-loader',
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
       },
     ],
   },
