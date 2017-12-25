d69ceb8e17f972dd834b278b3c8c633009b4a63f
Vitaliy <vkhotey@ya.ru>
2017-12-25 20:10:56 +0200

    Update webpack.config.js

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,4 +1,3 @@
-const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
@@ -6,7 +5,6 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
   entry: [
     'babel-polyfill',
-    'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',
     path.resolve(__dirname, '../src/index.js'),
   ],
   output: {
@@ -14,29 +12,19 @@ module.exports = {
     path: path.resolve(__dirname, '../dist'),
     publicPath: '/',
   },
-  devServer: {
-    historyApiFallback: true,
-  },
   devtool: 'inline-source-map',
   plugins: [
-    new CleanWebpackPlugin(['../dist']),
+    new CleanWebpackPlugin([path.resolve(__dirname, '../dist')]),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '../bin/index.html'),
       inject: false,
     }),
-    new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
   ],
   module: {
     rules: [
       {
-        test: /.css$/,
+        test: /\.css$/,
         use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader', 'eslint-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif|woff|woff2|eot|ttf|otf)$/,
@@ -45,9 +33,16 @@ module.exports = {
           {
             loader: 'url-loader',
             options: { limit: 40000 },
-          },
-          'image-webpack-loader',
-        ],
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: 'image-webpack-loader',
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
       },
     ],
   },
