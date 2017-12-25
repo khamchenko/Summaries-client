772498bf5da2781c090bdf4cc0d00ffac6b63bcb
Vitaliy <vkhotey@ya.ru>
2017-12-25 20:11:21 +0200

    Update server.js

--- bin/server.js
+++ bin/server.js
@@ -1,29 +1,25 @@
 const Koa = require('koa');
 const webpack = require('webpack');
-const koaWebpack = require('koa-webpack-middleware');
-const webpackHotMiddleware = require('webpack-dev-middleware');
+const koaWebpackMiddleware = require('koa-webpack-middleware');
 const path = require('path');
 const fs = require('fs');
+const config = require(path.resolve(__dirname, '../build/webpack.config'));
 
 const PORT = process.env.PORT || 3000;
 
 const app = new Koa();
-const config = require('../build/webpack.config');
-const indexHtmlPath = path.resolve(__dirname, './index.html');
 const compiler = webpack(config);
-const webpackMiddleware = koaWebpack.devMiddleware(compiler, {
+
+app.use(koaWebpackMiddleware.devMiddleware(compiler, {
   publicPath: config.output.publicPath,
   stats: { colors: true },
-  hot: true,
-});
+}));
 
-app.use(webpackMiddleware);
-app.use(koaWebpack.hotMiddleware(compiler));
-app.use((ctx) => {
+app.use(async (ctx) => {
   ctx.set('Content-Type', 'text/html');
-  ctx.body = fs.readFileSync(indexHtmlPath);
+  ctx.body = fs.readFileSync(path.resolve(__dirname, './index.html'));
 });
 
-app.listen(PORT, function () {
-  console.log(`Dev Server port: "${PORT}"`);
+app.listen(PORT, () => {
+  console.log(`Dev Server hosting on port: "${PORT}"`);
 });
