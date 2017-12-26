40a55cf022a2025249593090fc5ba79fe8f92573
Vitaliy <vkhotey@ya.ru>
2017-12-26 07:30:04 +0200

    Update webpack.config.js

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webapck');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
