6a6dfee902b5d254c73cde01abe746b21aed5426
vitaliy <khotey.v@ya.ru>
2018-02-28 16:22:34 +0200

    added eslint rules

--- src/components/SummariesList/SummariesItem/SummariesItem.js
+++ src/components/SummariesList/SummariesItem/SummariesItem.js
@@ -6,7 +6,16 @@ import ChevronRightIcon from 'rambler-ui/icons/forms/ChevronRightIcon';
 
 import './SummariesItem.css';
 
-const SummaryItem = ({ summary: { title, description, user: { firstName, lastName } } }) => (
+const SummaryItem = ({
+  summary: {
+    title,
+    description,
+    user: {
+      firstName,
+      lastName,
+    },
+  },
+}) => (
   <div className="summaries__summary summary">
     <div className="row">
       <div className="summary__img col-sm-2">
